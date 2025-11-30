1a77aa2294eb299a59aace7cef1b0cbb44439022
haakensonb <untamedhawk44@gmail.com>
2025-11-29 21:06:55 -0700

    Add color, fix vec3, integrate with main

--- vec3.h
+++ vec3.h
@@ -26,10 +26,10 @@ class vec3 {
             return *this;
         }
 
-        vec3& operator*=(const vec3& v) {
-            e[0] *= v.e[0];
-            e[1] *= v.e[1];
-            e[2] *= v.e[2];
+        vec3& operator*=(double t) {
+            e[0] *= t;
+            e[1] *= t;
+            e[2] *= t;
             return *this;
         }
 
